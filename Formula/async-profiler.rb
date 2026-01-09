432705936a4f36d010c182f2aa0862914d0d1cd6
Shitikanth Kashyap <shitikanth1@gmail.com>
2026-01-09 15:30:55 +0530

    Update async profiler to v4.0

--- Formula/async-profiler.rb
+++ Formula/async-profiler.rb
@@ -1,18 +1,18 @@
 class AsyncProfiler < Formula
   desc "Low overhead sampling profiler for Java"
   homepage "https://github.com/async-profiler/async-profiler"
-  url "https://github.com/async-profiler/async-profiler/releases/download/v3.0/async-profiler-3.0-macos.zip"
-  sha256 "cae4f207c33b964276385272a7cfe3750594cbcc8918f97a4bb67292bdbc43a8"
+  url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-macos.zip"
+  sha256 "1f27e5f5952ec40126a22addad6989bcbeb0509da8a2714ec489953abd698360"
   license "Apache-2.0"
 
   on_linux do
     on_intel do
-      url "https://github.com/async-profiler/async-profiler/releases/download/v3.0/async-profiler-3.0-linux-x64.tar.gz"
-      sha256 "2b631e8c0418e7e8a19d59a2d2eff016e87f8a4ffbac1ea395ec750b39dc3ce8"
+      url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-linux-x64.tar.gz"
+      sha256 "ba32bd69cc2906f6251cd5aa8ae72c3abc12267ba858d38733f7751cc4d3e05b"
     end
     on_arm do
-      url "https://github.com/async-profiler/async-profiler/releases/download/v3.0/async-profiler-3.0-linux-arm64.tar.gz"
-      sha256 "8d3b407055f66756eb9525a814cbcf119951a8fb82055df9471b6559326f7607"
+      url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-linux-arm64.tar.gz"
+      sha256 "482c142075dd8eeb762627087dd663725631e2747822f4480753e3b5507b3a62"
     end
   end
 
