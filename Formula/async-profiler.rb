d95a77a63f56ff211329cf81aa0dad9eacc8e717
Shitikanth Kashyap <shitikanth1@gmail.com>
2026-01-09 15:53:32 +0530

    Fix style issue

--- Formula/async-profiler.rb
+++ Formula/async-profiler.rb
@@ -1,12 +1,9 @@
 class AsyncProfiler < Formula
   desc "Low overhead sampling profiler for Java"
   homepage "https://github.com/async-profiler/async-profiler"
+  url "https://github.com/async-profiler/async-profiler/releases/download/v3.0/async-profiler-3.0-macos.zip"
+  sha256 "cae4f207c33b964276385272a7cfe3750594cbcc8918f97a4bb67292bdbc43a8"
   license "Apache-2.0"
-
-  on_macos do
-    url "https://github.com/async-profiler/async-profiler/releases/download/v3.0/async-profiler-3.0-macos.zip"
-    sha256 "cae4f207c33b964276385272a7cfe3750594cbcc8918f97a4bb67292bdbc43a8"
-  end
 
   on_linux do
     on_intel do
